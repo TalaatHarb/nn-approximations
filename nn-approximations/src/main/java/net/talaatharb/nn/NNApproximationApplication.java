4a8a6e5a39628f6d93d1331d96214a0124801a06
mharb <mohamed.harb@cegedim.com>
2024-04-19 03:03:27 +0200

    created initial project from template

---
+++ nn-approximations/src/main/java/net/talaatharb/nn/NNApproximationApplication.java
@@ -0,0 +1,10 @@
+package net.talaatharb.nn;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NNApproximationApplication{
+    public static void main(String[] args) {
+        log.info("Application Started");
+    }
+}
---
+++ nn-approximations/src/test/java/net/talaatharb/nn/NNApproximationApplicationTest.java
@@ -0,0 +1,14 @@
+package net.talaatharb.nn;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class NNApproximationApplicationTest {
+
+	@Test
+	void testProjectStarts() {
+		NNApproximationApplication.main(null);
+		assertTrue(true);
+	}
+}
